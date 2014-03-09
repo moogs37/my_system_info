cebff7247f46c81701a8284af9601b9963975f74
Dallas Smith <moogs@outlook.com>
2014-03-09 15:29:00 -0400

    Initial commit.
    This is the initial commit.

---
+++ My_System_Info/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace My_System_Info
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "My System Info v 0.1";
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
