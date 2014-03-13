57ca34e12b1aa8bcad2882d72d9bc473ec933e71
Dallas Smith <moogs@outlook.com>
2014-03-12 20:31:45 -0400

    Added Serial # And HD Size
    Used WMI to obtain serial # and HD Size

--- My_System_Info/Form1.Designer.cs
+++ My_System_Info/Form1.Designer.cs
@@ -28,21 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.SerialNumber = new System.Windows.Forms.Label();
+            this.HDDSize = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // SerialNumber
+            // 
+            this.SerialNumber.AutoSize = true;
+            this.SerialNumber.Location = new System.Drawing.Point(12, 45);
+            this.SerialNumber.Name = "SerialNumber";
+            this.SerialNumber.Size = new System.Drawing.Size(43, 13);
+            this.SerialNumber.TabIndex = 0;
+            this.SerialNumber.Text = "Serial #";
+            // 
+            // HDDSize
+            // 
+            this.HDDSize.AutoSize = true;
+            this.HDDSize.Location = new System.Drawing.Point(15, 76);
+            this.HDDSize.Name = "HDDSize";
+            this.HDDSize.Size = new System.Drawing.Size(51, 13);
+            this.HDDSize.TabIndex = 1;
+            this.HDDSize.Text = "HDDSize";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.HDDSize);
+            this.Controls.Add(this.SerialNumber);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label SerialNumber;
+        private System.Windows.Forms.Label HDDSize;
     }
 }
 
--- My_System_Info/Form1.cs
+++ My_System_Info/Form1.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Management;
+using System.Management.Instrumentation;
+
+
 
 namespace My_System_Info
 {
@@ -16,12 +20,30 @@ namespace My_System_Info
         {
             InitializeComponent();
             this.Text = "My System Info v 0.1";
-
+            SerialNumber.Text = "Serial Number: " + GetHDDSerialNumber("C");
+            HDDSize.Text = "HDD Size: " + getHDDSize("C");
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             
+        }
+        public string GetHDDSerialNumber(string drive)
+        {
+            //Assuming OS drive is on C, if not we would need this...
+            //if (drive == "" || drive == null)
+            //{
+            //    drive = "C";
+            //}
+            ManagementObject disk = new ManagementObject("Win32_LogicalDisk.DeviceID=\"" + drive + ":\"");
+            disk.Get();
+            return disk["VolumeSerialNumber"].ToString();
+        }
+        public string getHDDSize(string drive)
+        {
+            ManagementObject disk = new ManagementObject("Win32_LogicalDisk.DeviceID=\"" + drive + ":\"");
+            disk.Get();
+            return Convert.ToString(disk["Size"]);
         }
     }
 }
